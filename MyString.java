bc482d2b781e4a1f420d52c1036bf191ccd08c02
Itamar Berkowitz <itamar.berkowitz@post.runi.ac.il>
2024-11-29 22:44:27 +0200

    First 3 Questions

--- MyString.java
+++ MyString.java
@@ -18,13 +18,26 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String lowerString = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
+                lowerString += (char)(str.charAt(i) + 32);
+            }
+            else {
+                lowerString += str.charAt(i);
+            }
+        }
+        return lowerString;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        if (str2.length() > str1.length())
+            return false;
+        for (int i = 0; i <= (str1.length() - str2.length()); i++) {
+            if (str2.equals(str1.substring(i, i + str2.length())))
+                return true;
+        }
         return false;
     }
 }
